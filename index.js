d7cb2cae25aa35403c27e55cb383df66268fbe64
sandra <nyamweasandra@gmail.com>
2023-03-17 23:14:07 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const spy =(['Guadalupe','Ollie','Aki']);
+function writeCards(spy){
+    const surprise =[]
+    for(let i=O; < spy.length; i++) {
+        surprise. push('Thank you,${spy[i]}, for the wonderful surprise gift!')
+        console.log (surprise);
+    }
+    return surprise;
+}
+function countDown(){
+    for (letj= 10;j>=O;j--){
+        ccnsole.log(j)
+    }
+}
+countDown()
